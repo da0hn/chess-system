61130de19c1c56768ba30e16cad46e2a2f4f460c
Gabriel Honda <vegancraft13@gmail.com>
2020-01-18 13:14:15 -0400

    Camada Chess e print do tabuleiro

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,11 +1,15 @@
 package app;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
     public static void main(String[] args) {
-        Board board = new Board(8, 8);
+
+        ChessMatch match = new ChessMatch();
+
+        UI.printBoard(match.getPieces());
 
     }
 
